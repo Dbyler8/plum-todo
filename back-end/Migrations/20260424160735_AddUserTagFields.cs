845dc62fc164db1b7ab3e9a5f59b948e38c68455
Donovan Byler <61483657+Dbyler8@users.noreply.github.com>
2026-04-27 17:08:05 -0400

    Adding Database Migrations

---
+++ back-end/Migrations/20260424160735_AddUserTagFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace back_end.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTagFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tag",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TagColor",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TagColor",
+                table: "Users");
+        }
+    }
+}
---
+++ back-end/Migrations/20260424161236_MoveTagFieldsToTask.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace back_end.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveTagFieldsToTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TagColor",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tag",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TagColor",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "TagColor",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tag",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TagColor",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true);
+        }
+    }
+}
